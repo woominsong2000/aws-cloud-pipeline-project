773bf92172481f9adaf74828d45b09ed8c5bfb62
chikachik4 <wwwonwod0@gmail.com>
2026-03-14 21:00:17 +0900

    feat: network 모듈 VPC Endpoint 추가 및 region 변수화

--- Terraform/modules/network/outputs.tf
+++ Terraform/modules/network/outputs.tf
@@ -12,13 +12,3 @@ output "private_subnet_ids" {
   description = "List of IDs of private subnets"
   value       = aws_subnet.private[*].id
 }
-
-output "s3_bucket_name" {
-  description = "The name of the S3 bucket"
-  value       = aws_s3_bucket.storage.id
-}
-
-output "s3_bucket_arn" {
-  description = "The ARN of the S3 bucket"
-  value       = aws_s3_bucket.storage.arn
-}
