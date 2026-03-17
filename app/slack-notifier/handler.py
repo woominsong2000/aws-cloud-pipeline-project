f31df5b99272b218ba69cc6be7b28fe65c9f81e3
“woomin2021” <woomin2000@gmail.com>
2026-03-18 08:51:37 +0900

    fix ./ 파이썬 코드 수정

--- app/slack-notifier/handler.py
+++ app/slack-notifier/handler.py
@@ -4,21 +4,23 @@ import os
 
 http = urllib3.PoolManager()
 
-def handler(event, context):
-    # SNS에서 보낸 메시지 추출
+def handler(event, _context):
+    # 1. SNS 메시지 추출
     sns_message = event['Records'][0]['Sns']['Message']
 
-    # 슬랙에 보낼 데이터 포맷팅
+    # 2. 슬랙 메시지 포맷팅
     slack_body = {
         "text": f"🚨 *AWS 인프라 알람 발생!* 🚨\n\n> *내용:* {sns_message}",
         "username": "Reliability-Bot"
     }
 
-    # 슬랙 Webhook URL로 전송 (아까 복사한 URL로 대체하거나 환경변수 사용)
+    # 3. 테라폼에서 주입한 환경변수 읽기
     webhook_url = os.environ.get("SLACK_WEBHOOK_URL")
 
+    # 4. 슬랙으로 전송
     response = http.request(
-        'POST', url,
+        'POST',
+        webhook_url, # 변수 이름 일치 확인!
         body=json.dumps(slack_body),
         headers={'Content-Type': 'application/json'}
     )
